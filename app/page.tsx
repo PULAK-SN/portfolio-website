07ea2fa511ff1d43f2fae2b341e41098f403c66b
PULAK-SN <pulak.official22@gmail.com>
2025-07-28 00:24:51 +0530

    header added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main>
-      <h1>Portfolio</h1>
-    </main>
-  );
+  return <main></main>;
 }
--- lib/data.ts
+++ lib/data.ts
@@ -4,9 +4,9 @@ import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 
 // project thumbnail
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+// import corpcommentImg from "@/public/corpcomment.png";
+// import rmtdevImg from "@/public/rmtdev.png";
+// import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
@@ -77,21 +77,21 @@ export const projectsData = [
       "Convex",
       "Liveblocks",
     ],
-    imageUrl: corpcommentImg,
+    // imageUrl: corpcommentImg,
   },
   {
     title: "Real-Time Video Calling App",
     description:
       "A web-based video calling platform built using \textbf{Socket.IO, PeerJS}, and the \textbf{React Context API} to enable real-time, peer-to-peer video communication between users.",
     tags: ["HTML", "CSS", "React", "Next.Js", "WebRTC", "PeerJS"],
-    imageUrl: rmtdevImg,
+    // imageUrl: rmtdevImg,
   },
   {
     title: "GPT-3 Landing Page",
     description:
       " Developed a modern, responsive landing page inspired by OpenAI’s GPT-3, demonstrating clean UI design and component-based architecture. The project showcases front-end development skills and attention to detail. ",
     tags: ["HTML", "CSS", "React"],
-    imageUrl: wordanalyticsImg,
+    // imageUrl: wordanalyticsImg,
   },
 ] as const;
 

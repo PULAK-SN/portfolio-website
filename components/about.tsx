5e344c3507f19d2c865f72a834661c9b9c25db70
PULAK-SN <pulak.official22@gmail.com>
2025-11-08 01:56:21 +0530

    Project (School Dashboard) added

--- components/about.tsx
+++ components/about.tsx
@@ -14,8 +14,8 @@ const About = () => {
     >
       <SectionHeading>about me</SectionHeading>
 
-      <p className="mb-3">
-        After graduating with a degree in{" "}
+      <p className="mb-3 text-[18px] leading-relaxed md:leading-loose">
+        {/* After graduating with a degree in{" "}
         <span className="font-medium">Computer Science</span>, I decided to
         pursue my passion for programming. I enrolled in a coding bootcamp and
         learned <span className="font-medium">frontend web development</span>.{" "}
@@ -26,15 +26,32 @@ const About = () => {
         familiar with TypeScript and Prisma. I am always looking to learn new
         technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
-        developer.
+        developer. */}
+        After completing my Master’s degree in Computer Science from{" "}
+        <span className="font-medium">South Asian University, New Delhi</span> ,
+        I decided to fully pursue my passion for programming. I began my journey
+        with a coding bootcamp where I developed a strong foundation in{" "}
+        <span className="font-medium">frontend web development</span>. <br />
+        My core stack includes{" "}
+        <span className="font-medium">React and Next.js</span> , and I’m also
+        proficient with <span className="font-medium">TypeScript</span>. I’m
+        always eager to explore and learn new technologies to enhance my skills
+        and stay up to date with industry trends. Currently, I’m seeking a{" "}
+        <span className="font-medium">
+          full-time software developer position
+        </span>{" "}
+        where I can contribute, grow, and build impactful projects. <br />{" "}
+        <br />
+        When I’m not coding, you’ll probably find me watching movies or learning
+        something new — right now, I’m enjoying the process of learning the
+        <span className="font-medium"> Spanish language</span>.
       </p>
 
-      <p>
+      {/* <p>
         <span className="italic">When {`I'm`} not coding</span>, I enjoy
-        watching movies. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
+        watching movies. I also enjoy learning new things. I am currently
         learning <span className="font-medium">Spanish language</span>.
-      </p>
+      </p> */}
     </motion.section>
   );
 };
--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,5 @@
 import whiteBoard from "@/public/white-board.png";
+import school from "@/public/school-dashboard.png";
 import gpt3 from "@/public/gpt-3.png";
 import carePulse from "@/public/care-pulse.png";
 import restaurent from "@/public/restaurent.png";
@@ -28,9 +29,29 @@ export const links = [
 
 export const projectsData = [
   {
-    title: "White Board",
+    title: "School Dashboard",
     description:
-      "Developed a real-time collaborative whiteboard application using Next.js, Clerk, and Convex. The platform allows teams to draw, create sticky notes, and collaborate visually with secure authentication and instant update.",
+      "Architected a robust school management solution with a modular, role-based access control system, enabling secure authentication for admins, teachers, students, and parents via Clerk and JWT session management.",
+    tags: [
+      "React",
+      "TypeScript",
+      "Next.js",
+      "Prisma",
+      "Clerk",
+      "Zod (Form Validation)",
+      "Tailwind",
+    ],
+    links: {
+      liveLinks: "https://ps-school-management.vercel.app/",
+      githubLink: "https://github.com/PULAK-SN/school-management",
+    },
+    imageUrl: school,
+  },
+  {
+    title: "White Board",
+    description: `Developed a real-time collaborative whiteboard application using Next.js, Clerk, and Convex. 
+      The platform allows teams to draw, create sticky notes, and collaborate visually with secure 
+      authentication and instant update.`,
     tags: [
       "React",
       "TypeScript",
@@ -68,18 +89,6 @@ export const projectsData = [
     imageUrl: carePulse,
   },
   {
-    title: "GPT-3 Landing Page",
-    description: ` Developed a modern, responsive landing page inspired by OpenAI’s GPT-3, demonstrating
-     clean UI design and component-based architecture. The project showcases front-end development skills
-      and attention to detail.`,
-    tags: ["HTML", "CSS", "JavaScript", "React"],
-    links: {
-      liveLinks: "https://gpt-3-zeta-nine.vercel.app/",
-      githubLink: "https://github.com/PULAK-SN/GPT3",
-    },
-    imageUrl: gpt3,
-  },
-  {
     title: "Restaurent Application",
     description: `This project is a visually appealing restaurant application landing page. Built using React, 
     it demonstrates skills in front-end development, showcasing a clean and responsive design. 
@@ -91,6 +100,18 @@ export const projectsData = [
       githubLink: "https://github.com/PULAK-SN/Restaurant-Application",
     },
     imageUrl: restaurent,
+  },
+  {
+    title: "GPT-3 Landing Page",
+    description: ` Developed a modern, responsive landing page inspired by OpenAI’s GPT-3, demonstrating
+     clean UI design and component-based architecture. The project showcases front-end development skills
+      and attention to detail.`,
+    tags: ["HTML", "CSS", "JavaScript", "React"],
+    links: {
+      liveLinks: "https://gpt-3-zeta-nine.vercel.app/",
+      githubLink: "https://github.com/PULAK-SN/GPT3",
+    },
+    imageUrl: gpt3,
   },
 ] as const;
 
